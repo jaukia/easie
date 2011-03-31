ff24e68101aa6eef7b712c25b76a1873ecf12d5a
jaukia <janne.aukia@gmail.com>
2011-03-31 22:00:23 +0300

    Renamed file

--- jquery.easie.js
+++ jquery.easie.js
@@ -1,8 +1,8 @@
-// example of use $(elem).animate( {top: 100}, $.eazer(.25,.1,.25,1) );
+// example of use $(elem).animate( {top: 100}, $.easie(.25,.1,.25,1) );
 
 /*
- * jquery.eazer.js:
- * http://www.github.com/jaukia/eazer
+ * jquery.easie.js:
+ * http://www.github.com/jaukia/easie
  *
  * Version history:
  * 0.01 xxx
@@ -30,7 +30,7 @@
 (function($) {
     "use strict";
 
-    $.eazer = function(p1x,p1y,p2x,p2y,name) {
+    $.easie = function(p1x,p1y,p2x,p2y,name) {
         name = name || "cubic-"+Array.prototype.join.call(arguments,"-");
         if ( !$.easing[name] ) {
             var cubicBezierAtTimeLookup = makeLookup(function(p) {
@@ -46,11 +46,11 @@
         return easingName;
     }
 
-    $.eazer(0.0,0.0,1.0,1.0,  "cubic-linear");
-    $.eazer(0.25,0.1,0.25,1.0,"cubic-ease");
-    $.eazer(0.42,0.0,1.0,1.0, "cubic-ease-in");
-    $.eazer(0.0,0.0,0.58,1.0, "cubic-ease-out");
-    $.eazer(0.42,0.0,0.58,1.0,"cubic-ease-in-out");
+    $.easie(0.0,0.0,1.0,1.0,  "cubic-linear");
+    $.easie(0.25,0.1,0.25,1.0,"cubic-ease");
+    $.easie(0.42,0.0,1.0,1.0, "cubic-ease-in");
+    $.easie(0.0,0.0,0.58,1.0, "cubic-ease-out");
+    $.easie(0.42,0.0,0.58,1.0,"cubic-ease-in-out");
 
     function makeLookup(func,steps) {
         var i;
